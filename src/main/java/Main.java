270aba537da80997917f4a491577e335b6988f0d
GZWX <zhangchao19932008@163.com>
2020-08-27 16:33:24 +0800

    my first github project

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author czhang@mindpointeye.com
+ * @version 1.0
+ * @Date 2020/8/27 14:57
+ * @descrption
+ */
+public class Main {
+    public static void main(String[] args) {
+        TankFrame tf=new TankFrame();
+    }
+}
---
+++ src/main/java/TankFrame.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @author czhang@mindpointeye.com
+ * @version 1.0
+ * @Date 2020/8/27 15:09
+ * @descrption
+ */
+public class TankFrame extends Frame {
+    int x = 200, y = 200;
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        System.out.println("paint");
+        g.fillRect(x, y, 50, 50);
+        x += 10;
+        y += 10;
+    }
+}
